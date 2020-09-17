046598e78afce9bebebffa98d06eac2acf8c8021
gilesrobert <gilesrobert5454@gmail.com>
2020-09-17 12:51:48 -0400

    Changed location and color

--- TestProject/sketch.js
+++ TestProject/sketch.js
@@ -2,14 +2,14 @@
 function setup() {
   // put setup code here
 createCanvas(200,200);
-background(200,10,120);
+background(120,100,120);
 
 }
 
 function draw() {
   //Put draw code here
   fill(30,40,200);
-  circle(100,100,70);
+  circle(200,0,70);
 
 
 }
